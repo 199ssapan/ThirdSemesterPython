957ce654e247adcc5e1d35cf67f39111c0fc57e1
PetrovMaksim2004 <116202146+PetrovMaksim2004@users.noreply.github.com>
2023-10-02 23:48:56 +0500

    fin

--- parseLinks/newtask/__main__.py
+++ parseLinks/newtask/__main__.py
@@ -1,39 +1,28 @@
+"""Lesson task """
 import requests
 from bs4 import BeautifulSoup
 
-
-# resp = requests.get("https://ru.wikipedia.org/wiki/Python")
-
-# bsobj = BeautifulSoup(resp.text, "html.parser")
-
-# refs = bsobj.find_all("a")
-# for ref in refs:
-#     try:
-#         print(ref['href'])
-#     except KeyError:
-#         continue
-    
 def find_link_depth(url, target_page, depth):
+    """ Function that tests the theory of n handshakes, only for wikipedia """
     if depth == 0:
         return False
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         soup = BeautifulSoup(response.text, 'html.parser')
         links = soup.find_all('a', href=True)
         for link in links:
             next_url = link['href']
             print(next_url)
-            if "/wiki/" in next_url:
+            if "/wiki/" in next_url and "http" not in next_url:
                 next_page = "https://ru.wikipedia.org" + next_url
-                # print(next_page)
                 if next_page == target_page:
                     print(f"Достигнута целевая страница: {target_page}")
                     return True
-                elif find_link_depth(next_page, target_page, depth - 1):
-                    return True
-    except Exception:
+                find_link_depth(next_page, target_page, depth - 1)
+    except KeyError:
         pass
     return False
 
-
-print(find_link_depth("https://ru.wikipedia.org/wiki/Python", "https://ru.wikipedia.org/wiki/Cython", 2))
+print(find_link_depth("https://ru.wikipedia.org/wiki/Python",
+                       "https://ru.wikipedia.org/wiki/Cython",
+                         2))
