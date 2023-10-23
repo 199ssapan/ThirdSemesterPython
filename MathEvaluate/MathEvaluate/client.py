e557212dd7c6c932c9b858abd6e68a2a7dd1e1eb
PetrovMaksim2004 <116202146+PetrovMaksim2004@users.noreply.github.com>
2023-10-23 22:08:30 +0500

    s

--- MathEvaluate/MathEvaluate/client.py
+++ MathEvaluate/MathEvaluate/client.py
@@ -5,7 +5,7 @@ import math_pb2
 def run():
     channel = grpc.insecure_channel("127.0.0.1:50051")
     stub = math_pb2_grpc.evaluatorStub(channel)
-    response = stub.evaluate(math_pb2.strRequest(expression="10 + 21"))
+    response = stub.evaluate(math_pb2.strRequest(expression="100 * 100_000"))
     print(response.number)
 
 if __name__ == "__main__":
